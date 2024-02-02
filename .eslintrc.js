97bd49b6d23aadecad7bd2dbea3caca5f82e38f2
danilo neves cruz <cruzdanilo@gmail.com>
2024-02-02 16:23:53 -0300

    🔨 package: use bun, upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,46 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  parserOptions: { project: ['tsconfig.json', 'tsconfig.dev.json'] },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["tsconfig.json", "tsconfig.dev.json"] },
+  settings: {
+    "import/resolver": { typescript: true },
+    jest: { version: "latest" },
+  },
   extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:eslint-comments/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:eslint-comments/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "plugin:unicorn/recommended",
   ],
   rules: {
-    yoda: 'off',
-    'no-bitwise': 'off',
-    'no-console': 'off',
-    'no-param-reassign': 'off',
-    'no-nested-ternary': 'off',
-    'eslint-comments/no-unused-disable': 'error',
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-import-type-side-effects": "error",
+    "@typescript-eslint/no-shadow": "error",
+    "eslint-comments/no-unused-disable": "error",
+    "import/prefer-default-export": "error",
+    "no-console": "error",
+    "no-shadow": "off", // @typescript-eslint/no-shadow
+    "unicorn/filename-case": "off", // use default export name
+    "unicorn/number-literal-case": "off", // incompatible with prettier
   },
   overrides: [
+    { files: "src/**/*", extends: "plugin:require-extensions/recommended" },
+    { files: "test/**/*", extends: "plugin:jest/recommended" },
     {
-      files: ['**/.eslintrc.js'],
-      extends: ['plugin:node/recommended'],
+      files: [".eslintrc.*", "test/**/*"],
+      extends: "plugin:node/recommended",
       rules: {
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        "import/no-unresolved": ["error", { ignore: ["^bun:"] }],
+        "node/no-missing-import": "off",
+        "unicorn/prefer-module": "off",
       },
+      overrides: [
+        { files: "*.ts", rules: { "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }] } },
+      ],
     },
   ],
+  ignorePatterns: ["node_modules/", "esm/", "cjs/", "types/"],
 };

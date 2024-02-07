80d0e1dc2df57bb4e3795a7381762cd156598452
danilo neves cruz <cruzdanilo@gmail.com>
2024-02-06 22:00:10 -0300

    ⚡️ installments: reuse irm intermediary value

--- src/installments/fromAmounts.ts
+++ src/installments/fromAmounts.ts
@@ -1,4 +1,5 @@
-import WAD, { SQ_WAD } from "../fixed-point-math/WAD.js";
+import sqrt from "../fixed-point-math/sqrt.js";
+import WAD, { SQ_WAD, TWO_WAD } from "../fixed-point-math/WAD.js";
 import fixedRate, { INTERVAL, type IRMParameters } from "../interest-rate-model/fixedRate.js";
 
 export default function fromAmounts(
@@ -9,9 +10,13 @@ export default function fromAmounts(
   uFixed: readonly bigint[],
   uFloating: bigint,
   uGlobal: bigint,
-  irmParameters: IRMParameters,
+  parameters: IRMParameters,
   timestamp: number,
 ) {
+  const sqFNatPools = (BigInt(maxPools) * SQ_WAD) / parameters.fixedAllocation;
+  const fNatPools = sqrt(sqFNatPools * WAD);
+  const natPools = ((TWO_WAD - sqFNatPools) * SQ_WAD) / (fNatPools * (WAD - fNatPools));
+
   let uGlobalAfter = uGlobal;
   return uFixed.map((uFixedBefore, index) => {
     const amount = amounts[index]!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
@@ -20,7 +25,7 @@ export default function fromAmounts(
       (BigInt(maturity - timestamp) * WAD) / BigInt(timestamp + maxPools * INTERVAL - (timestamp % INTERVAL));
     const uFixedAfter = uFixedBefore + (amount * WAD - 1n) / totalAssets + 1n;
     uGlobalAfter += (amount * WAD - 1n) / totalAssets + 1n;
-    const rate = fixedRate(maturity, maxPools, uFixedAfter, uFloating, uGlobalAfter, irmParameters, timestamp);
+    const rate = fixedRate(maturity, maxPools, uFixedAfter, uFloating, uGlobalAfter, parameters, timestamp, natPools);
     return amount + (amount * rate * maturityFactor) / SQ_WAD;
   });
 }

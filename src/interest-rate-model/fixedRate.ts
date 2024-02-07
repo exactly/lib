b451316a3966a93f17d723637a2d9bc762b75542
danilo neves cruz <cruzdanilo@gmail.com>
2024-02-06 21:19:35 -0300

    ⚡️ irm: allow caching of intermediary values

--- src/interest-rate-model/fixedRate.ts
+++ src/interest-rate-model/fixedRate.ts
@@ -4,7 +4,6 @@ import lnWad from "../fixed-point-math/lnWad.js";
 import sqrt from "../fixed-point-math/sqrt.js";
 import type IRMParameters from "./Parameters.d.ts";
 import baseRate from "./baseRate.js";
-import floatingRate from "./floatingRate.js";
 
 export const INTERVAL = 4 * 7 * 86_400;
 
@@ -16,18 +15,23 @@ export default function fixedRate(
   uGlobal: bigint,
   parameters: IRMParameters,
   timestamp = Date.now() / 1000,
+  natPools?: bigint,
+  base = baseRate(uFloating, uGlobal, parameters),
 ) {
+  const { spreadFactor, maturitySpeed, timePreference, fixedAllocation, maxRate } = parameters;
+
   if (timestamp >= maturity) throw new Error("ALREADY_MATURED");
   if (uFixed > uGlobal) throw new Error("UTILIZATION_EXCEEDED");
-  if (uFixed === 0n) return floatingRate(uFloating, uGlobal, parameters);
+  if (uFixed === 0n) return base > maxRate ? maxRate : base;
 
-  const { spreadFactor, maturitySpeed, timePreference, fixedAllocation, maxRate } = parameters;
-  const sqFNatPools = (BigInt(maxPools) * SQ_WAD) / fixedAllocation;
-  const fNatPools = sqrt(sqFNatPools * WAD);
   const fixedFactor = (BigInt(maxPools) * uFixed * SQ_WAD) / (uGlobal * fixedAllocation);
-  const natPools = ((TWO_WAD - sqFNatPools) * SQ_WAD) / (fNatPools * (WAD - fNatPools));
   const maturityFactor =
     (BigInt(maturity - timestamp) * WAD) / BigInt(timestamp + maxPools * INTERVAL - (timestamp % INTERVAL));
+  if (natPools == undefined) {
+    const sqFNatPools = (BigInt(maxPools) * SQ_WAD) / fixedAllocation;
+    const fNatPools = sqrt(sqFNatPools * WAD);
+    natPools = ((TWO_WAD - sqFNatPools) * SQ_WAD) / (fNatPools * (WAD - fNatPools));
+  }
 
   const spread =
     WAD +
@@ -37,7 +41,6 @@ export default function fixedRate(
           WAD)) /
       WAD;
 
-  const base = baseRate(uFloating, uGlobal, parameters);
   if (base >= (maxRate * WAD) / spread) return maxRate;
   return (base * spread - 1n) / WAD + 1n;
 }

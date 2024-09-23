ff0123c78ba8453afa5bb2fc37641ec59e804a5c
itofarina <farinalvaro@gmail.com>
2024-09-23 17:55:32 -0300

    ✅ auditor: fix tests with enabled markets

--- test/auditor.test.ts
+++ test/auditor.test.ts
@@ -6,6 +6,7 @@ import healthFactor from "../src/auditor/healthFactor.js";
 import divWad from "../src/fixed-point-math/divWad.js";
 import divWadUp from "../src/fixed-point-math/divWadUp.js";
 import mulDiv from "../src/fixed-point-math/mulDiv.js";
+import mulWad from "../src/fixed-point-math/mulWad.js";
 import { INTERVAL } from "../src/interest-rate-model/fixedRate.js";
 
 describe("auditor", () => {
@@ -27,7 +28,9 @@ describe("auditor", () => {
 function exactlyAccountLiquidity(): { collateral: bigint; debt: bigint } {
   const usdcBaseUnit = 10n ** 6n;
   return {
-    collateral: mulDiv(parseUnits("10000", 6), parseUnits("0.91", 18), usdcBaseUnit),
+    collateral:
+      mulDiv(parseUnits("10000", 6), parseUnits("0.91", 18), usdcBaseUnit) +
+      mulDiv(mulWad(parseUnits("1", 18), parseUnits("2500", 18)), parseUnits("0.86", 18), 10n ** 18n),
     debt:
       divWadUp(parseUnits("2000", 18), parseUnits("0.91", 18)) +
       divWadUp(parseUnits("5000", 18), parseUnits("0.86", 18)),
@@ -37,7 +40,7 @@ function exactlyAccountLiquidity(): { collateral: bigint; debt: bigint } {
 const exactly = {
   marketsData: [
     {
-      market: "",
+      market: "0x6926B434CCe9b5b7966aE1BfEef6D0A7DCF3A8bb",
       decimals: 6,
       usdPrice: parseUnits("1", 18),
       adjustFactor: parseUnits("0.91", 18),
@@ -48,11 +51,11 @@ const exactly = {
       penaltyRate: parseUnits("0.000000052083333333", 18),
     },
     {
-      market: "",
+      market: "0xc4d4500326981eacD020e20A81b1c479c161c7EF",
       decimals: 18,
       usdPrice: parseUnits("2500", 18),
       adjustFactor: parseUnits("0.86", 18),
-      isCollateral: false,
+      isCollateral: true,
       floatingBorrowAssets: parseUnits("1", 18),
       floatingDepositAssets: parseUnits("1", 18),
       fixedBorrowPositions: [{ principal: parseUnits("0.9", 18), fee: parseUnits("0.1", 18), maturity: INTERVAL }],

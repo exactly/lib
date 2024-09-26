f112a88b040a70045ae26a271818b413af515488
danilo neves cruz <cruzdanilo@gmail.com>
2024-09-25 21:14:38 -0300

    🥅 auditor: handle zero debt

--- src/auditor/healthFactor.ts
+++ src/auditor/healthFactor.ts
@@ -1,7 +1,8 @@
 import accountLiquidity, { type AccountLiquidityData } from "./accountLiquidity.js";
+import MAX_UINT256 from "../fixed-point-math/MAX_UINT256.js";
 import divWad from "../fixed-point-math/divWad.js";
 
 export default function healthFactor(data: AccountLiquidityData, timestamp = Math.floor(Date.now() / 1000)): bigint {
   const { adjCollateral, adjDebt } = accountLiquidity(data, timestamp);
-  return divWad(adjCollateral, adjDebt);
+  return adjDebt ? divWad(adjCollateral, adjDebt) : MAX_UINT256;
 }

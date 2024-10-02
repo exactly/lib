4a0d09e55a50672ef3e4213b5259b99f0d5e1a16
danilo neves cruz <cruzdanilo@gmail.com>
2024-10-02 14:46:09 -0300

    ✨ package: export new functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+export { default as accountLiquidity } from "./auditor/accountLiquidity.js";
+export { default as borrowLimit } from "./auditor/borrowLimit.js";
+export { default as healthFactor } from "./auditor/healthFactor.js";
+export { default as withdrawLimit } from "./auditor/withdrawLimit.js";
+
+export { default as divWad } from "./fixed-point-math/divWad.js";
+export { default as divWadUp } from "./fixed-point-math/divWadUp.js";
 export { default as expWad } from "./fixed-point-math/expWad.js";
 export { default as lnWad } from "./fixed-point-math/lnWad.js";
 export { default as log2 } from "./fixed-point-math/log2.js";
 export { default as MAX_UINT256 } from "./fixed-point-math/MAX_UINT256.js";
+export { default as max } from "./fixed-point-math/max.js";
+export { default as min } from "./fixed-point-math/min.js";
+export { default as mulDiv } from "./fixed-point-math/mulDiv.js";
+export { default as mulDivUp } from "./fixed-point-math/mulDivUp.js";
+export { default as mulWad } from "./fixed-point-math/mulWad.js";
+export { default as mulWadUp } from "./fixed-point-math/mulWadUp.js";
 export { default as sqrt } from "./fixed-point-math/sqrt.js";
 export { default as WAD } from "./fixed-point-math/WAD.js";
 
@@ -16,16 +29,19 @@ export { default as floatingUtilization } from "./interest-rate-model/floatingUt
 export { default as globalUtilization } from "./interest-rate-model/globalUtilization.js";
 export type { default as IRMParameters, IRMFloatingParameters } from "./interest-rate-model/Parameters.ts";
 
+export { default as floatingDepositRates } from "./market/floatingDepositRates.js";
+
 export { default as abs } from "./vector/abs.js";
 export { default as add } from "./vector/add.js";
 export { default as fill } from "./vector/fill.js";
 export { default as map2 } from "./vector/map2.js";
 export { default as map3 } from "./vector/map3.js";
-export { default as max } from "./vector/max.js";
+export { default as maxV } from "./vector/max.js";
 export { default as mean } from "./vector/mean.js";
-export { default as min } from "./vector/min.js";
-export { default as mulDiv } from "./vector/mulDiv.js";
-export { default as mulDivUp } from "./vector/mulDivUp.js";
+export { default as minV } from "./vector/min.js";
+export { default as mul } from "./vector/mul.js";
+export { default as mulDivV } from "./vector/mulDiv.js";
+export { default as mulDivUpV } from "./vector/mulDivUp.js";
 export { default as powDiv } from "./vector/powDiv.js";
 export { default as sub } from "./vector/sub.js";
 export { default as sum } from "./vector/sum.js";
